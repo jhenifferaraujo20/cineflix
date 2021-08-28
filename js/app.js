218d34dc67f843afc33d7da02b9e3884e5bbbdab
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2021-08-27 21:52:43 -0300

    Refatoração do código

--- js/app.js
+++ js/app.js
@@ -1,47 +1,32 @@
-const API_KEY = "api_key=279f9c04a9fb558091da42e646a3e5e2";
 const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "api_key=279f9c04a9fb558091da42e646a3e5e2";
+const API_URL_POPULAR = BASE_URL + "movie/popular?" + API_KEY + "&language=pt-BR&page=1";
+const API_URL_SEARCH = BASE_URL + "search/movie?" + API_KEY + "&language=pt-BR&query=";
+/*const IMG_URL = "https://image.tmdb.org/t/p/w500/";*/
+const IMG_URL = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/";
+const BASE_LINK = "https://www.themoviedb.org/movie/";
 
-function moviesAPI() {
-    const urlAPI = BASE_URL + "movie/popular?" + API_KEY + "&language=pt-BR&page=1";
-
-    $.ajax({
-        url: urlAPI,
-        async: false,
-        error: function (erro) {
-            console.log("Erro ao consultar API:");
-            console.log(erro.responseJSON.message);
-        },
-        success: function (dados) {
-            movies = dados.results;
-        },
+function getMovies(url) {
+    fetch(url).then((response) => response.json()).then(data => {
+        LoadMovies(data.results);
     });
 }
 
-moviesAPI();
-
-function MovieCart(image, name, rating, link) {
-    let cart = "";
-    cart += "<div class='movie-cart col-sm-6 col-md-4 col-lg-3 mb-2'>";
-    cart += "<a href='" + link + "' target = 'a_blank'>";
-    cart += "<img src='" + image + "'>";
-    cart += "<p>" + name + "<br>Nota: " + rating + "</p></a>";
-    cart += "</div>";
-    return cart;
-}
-
-function LoadMovies() {
-    let urlImg = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/";
-    let preLink = "https://www.themoviedb.org/movie/";
-
+function LoadMovies(data) {
     let moviesList = document.querySelector(".movies-list");
-    let carts = "";
-    movies.forEach(movie => {
-        link = preLink + movie.id;
-        carts += MovieCart(urlImg + movie.poster_path, movie.title, movie.vote_average, link)
+    
+    data.forEach(movie => {
+        linkMovieDetail = BASE_LINK + movie.id;
+
+        moviesList.innerHTML += `
+            <div class="movie-card col-6 col-md-4 col-lg-3 mb-2">
+                <a href="${linkMovieDetail}" target="a_blank">
+                    <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
+                    <p>${movie.title}<br>Tmdb:  ${movie.vote_average}</p>
+                </a>
+            </div>
+        `;
     });
-    moviesList.innerHTML = carts;
 }
 
-window.onload = function () {
-    LoadMovies();
-}
+window.addEventListener('DOMContentLoaded', getMovies(API_URL_POPULAR));
